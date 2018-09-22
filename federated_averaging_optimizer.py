4bbc2bc8082addc142bb9c9f49e0b077e5a52b6d
Iker <iker@acuratio.com>
2018-09-21 19:38:06 -0700

    Branding correction

--- federated_averaging_optimizer.py
+++ federated_averaging_optimizer.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-# Modifications copyright (C) 2018 CoMind.
+# Modifications copyright (C) 2018 coMind.
 # ==============================================================================
 
 """Synchronize replicas for FedAvg training."""
