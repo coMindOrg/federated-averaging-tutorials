3966e9be895a1e499d3a241fba5dd9ccda186b38
Iker <iker@acuratio.com>
2018-09-25 15:24:23 -0700

    Minor improvements
    Correct typo and use sha1 as we just need it to guarantee the integrity and not actual authentication

--- federated-sockets/config.py
+++ federated-sockets/config.py
@@ -2,12 +2,12 @@ import hashlib
 
 SEND_RECEIVE_CONF = lambda x: x
 SEND_RECEIVE_CONF.key = b'4C5jwen4wpNEjBeq1YmdBayIQ1oD'
-SEND_RECEIVE_CONF.hashfunction = hashlib.sha512
-SEND_RECEIVE_CONF.hashsize = int(512 / 8)
+SEND_RECEIVE_CONF.hashfunction = hashlib.sha1
+SEND_RECEIVE_CONF.hashsize = int(160 / 8)
 SEND_RECEIVE_CONF.error = b'error'
 SEND_RECEIVE_CONF.recv = b'reciv'
 SEND_RECEIVE_CONF.signal = b'go!go!go!'
-SEND_RECEIVE_CONF.buffer = 8192**2
+SEND_RECEIVE_CONF.buffer = 8192*2
 
 SSL_CONF = lambda x: x
 SSL_CONF.key_path = 'server.key'
